6e8fb8b4796e8dfcb713cdd982777115bb51a35f
pigdragon <dragonpig@nate.com>
2020-01-04 10:32:57 +0900

    Project Rock!!!

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Project Diva");
+        System.out.println("Project Rock");
     }
 }
